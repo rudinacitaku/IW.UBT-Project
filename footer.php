33c403716519dddebef70182d3bdd32cd5050306
rudinacitaku <120095553+rudinacitaku@users.noreply.github.com>
2023-02-26 23:57:39 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class='footer'>
+<div class='border'>
+	<p class="text-center"> Online Newspaper All Rights Reserved 2023 </p>
+</div>
+</div>
+
+</body>
+</html>
---
+++ headery.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Online Newspaper</title>
+	<link rel="stylesheet" href="index.css">
+</head>
+<body>
+	<h1> Admin Panel </h1>
+
+		<div class='headery text-center'>
+		<div class='border'>
+			<ul> 
+				<li><a href="index.php"> Home </a> </li>
+				<li><a href="manage-admin.php"> Admin</a> </li>
+				<li><a href="manage-category.php"> Category </a> </li>
+				<li><a href="manage-news.php"> News </a> </li>
+			</ul>
+</div>
+</div>
